7b81d3f1c3e09818c5d4fce9ab94c6678cd9c079
Romel Niño Paano <rpaano02@gmail.com>
2019-11-10 14:40:29 +0800

    creating new view post

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,9 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/post/create', [
+
+    'uses' => 'PostsController@create',
+    'as' => 'post.create'
+]);
